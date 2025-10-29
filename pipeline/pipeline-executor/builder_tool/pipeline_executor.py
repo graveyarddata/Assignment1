fdbd8e79a36aa15d336b3ce266bf0d60df535992
graveyarddata <Larskerkhof03@gmail.com>
2025-10-29 09:26:58 +0100

    Refactor pipeline_executor to parse command line args

--- pipeline/pipeline-executor/builder_tool/pipeline_executor.py
+++ pipeline/pipeline-executor/builder_tool/pipeline_executor.py
@@ -2,14 +2,12 @@ import argparse
 import json
 import logging
 import sys
-from datetime import datetime
-from typing import Any, Dict, Optional
 
 import google.cloud.aiplatform as aip
 
 
 def run_pipeline_job(name, pipeline_def, pipeline_root, parameter_dict):
-  """This runs the pipeline, similar to last part of pipeline"""
+    # Opening JSON file
     f = open(parameter_dict)
     data = json.load(f)
     print(data)
@@ -22,4 +20,17 @@ def run_pipeline_job(name, pipeline_def, pipeline_root, parameter_dict):
         parameter_values=data)
     job.run()
 
-def parse_command_line_arguments()
+
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, help="Pipeline Name")
+    parser.add_argument('--pipeline_def', type=str, default="pipeline.yaml", help="Pipeline YAML definition file")
+    parser.add_argument('--pipeline_root', type=str, help="GCP bucket for pipeline_root")
+    parser.add_argument('--parameter_dict', type=str, help="Pipeline parameters as a josn file")
+    args = parser.parse_args()
+    return vars(args)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    run_pipeline_job(**parse_command_line_arguments())
